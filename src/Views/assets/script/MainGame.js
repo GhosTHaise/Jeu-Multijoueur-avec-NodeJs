9db3f60e00391e5a848bae0f0785e6dec77e6010
GhosTHaise <Ghostrex2@gmail.com>
2022-05-24 10:04:36 +0300

    Dynamiser le jeu

--- src/Views/assets/script/MainGame.js
+++ src/Views/assets/script/MainGame.js
@@ -26,6 +26,11 @@ const randomFood = () => {
                             y : Math.floor(Math.random() * HEIGHT / GRID_SIZE ) * GRID_SIZE
                         }   
 }
+//Convertir les coordonnees en String
+const coordToString = (obj) => {
+    return [obj.x,obj.y].join(',');
+}
+
 const tick = () => {
     var snakePart = {x : snake[0].x,y : snake[0].y};
     var axis = (Math.abs(direction) === 1) ? 'x' : 'y';
@@ -59,8 +64,12 @@ if(snakePart.x < 0 || snakePart.x > WIDTH || snakePart.y < 0 || snakePart.y > HE
     end = true;
 }
 //draw the snake
+var i =0,
+    snakeObj = {};
 for(let snakeBody of snake){
     ctx.fillRect(snakeBody.x,snakeBody.y,GRID_SIZE-1,GRID_SIZE -1);
-
+    if(i > 0) snakeObj[coordToString(snakeBody)] == true;
+    i++;
 }
+if(snakeObj[coordToString(snakePart)]) end = true;
 }
