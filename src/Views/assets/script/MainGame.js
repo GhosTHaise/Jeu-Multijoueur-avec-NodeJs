18a468c4fc8b7dfde2ede493ea2d18fbc09816a7
GhosTHaise <Ghostrex2@gmail.com>
2022-05-24 08:32:42 +0300

    Initialisation screen grid

--- src/Views/assets/script/MainGame.js
+++ src/Views/assets/script/MainGame.js
@@ -1,3 +1,12 @@
 "use strict"
 const cvs = document.getElementById('MainCanvas');
 const ctx = cvs.getContext('2d');
+
+//initialiser le grid
+const WIDTH = cvs.width;
+const HEIGHT = cvs.height;
+const NOMBRE_GRID = 5000;
+const GRID_SIZE = Math.sqrt((WIDTH*HEIGHT)/NOMBRE_GRID);
+//alert("width : "+WIDTH+" | height : "+HEIGHT+" | grid_size : "+GRID_SIZE);
+ctx.scale(2,2);
+//Snake 
