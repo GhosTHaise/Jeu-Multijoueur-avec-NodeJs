19e822c0c01d6054a7fabb2354e331c21b3ef4b6
GhosTHaise <Ghostrex2@gmail.com>
2022-05-24 08:49:24 +0300

    Initialisation Snake

--- src/Views/assets/script/MainGame.js
+++ src/Views/assets/script/MainGame.js
@@ -3,10 +3,17 @@ const cvs = document.getElementById('MainCanvas');
 const ctx = cvs.getContext('2d');
 
 //initialiser le grid
-const WIDTH = cvs.width;
-const HEIGHT = cvs.height;
-const NOMBRE_GRID = 5000;
-const GRID_SIZE = Math.sqrt((WIDTH*HEIGHT)/NOMBRE_GRID);
+const WIDTH = cvs.width,
+      HEIGHT = cvs.height,
+      NOMBRE_GRID = 5000,
+      GRID_SIZE = Math.sqrt((WIDTH*HEIGHT)/NOMBRE_GRID);
 //alert("width : "+WIDTH+" | height : "+HEIGHT+" | grid_size : "+GRID_SIZE);
 ctx.scale(2,2);
 //Snake 
+var direction = newDirection = 1,
+    snakeLength = 5,
+    snake = [{x : WIDTH / 2,y : HEIGHT / 2}],
+    food = null,
+    end = false,
+    score = 0
+
