e965a8f59837fa377d079e2ae4f9bd37ae24a874
GhosTHaise <Ghostrex2@gmail.com>
2022-05-25 09:54:22 +0300

    Fix site icon problem

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.use("/home",require("./src/Route/home"));
 //Asset  - Icon
 app.use("/icon",express.static("./src/Views/assets/Icon"));
 //
+app.get("./favicon.ico",(req,res) => {
+    res.sendFile("/icon/icons8-ghost-64.png")
+})
+
 app.listen(port,()=>{
     console.log("application start on http://localhost:"+port);
 })
